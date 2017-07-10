a777e73fd66c3a0334a3519325db37694fabd2c9
Sebastian Mense <mense@rhrk.uni-kl.de>
2017-07-10 16:38:42 +0200

    Added transformation to update value per-frame using easing methods

---
+++ osuweb/js/game/scenes/transformation.js
@@ -0,0 +1,115 @@
+"use strict";
+
+import {MathUtil} from "../../util/mathutil";
+
+export class TransformationManager {
+    constructor() {
+        this._transformations = [];
+    }
+
+    addTransformation(transformation) {
+        this._transformations.push(transformation);
+    }
+
+    update() {
+        for(let key in this._transformations) {
+            if(this._transformations[key].update()) {
+                if(this._transformations[key].callback) this._transformations[key].callback();
+                delete this._transformations[key];
+            }
+        }
+    }
+}
+
+export let TRANSFORMATION_MANAGER = new TransformationManager();
+
+export class Transformation {
+    constructor(element, start, destination, duration, callback = null, unit = "%", easingType = "linear") {
+        this.startTime = window.performance.now();
+        this.duration = duration;
+        this.start = start;
+        this.destination = destination;
+        this.element = element;
+
+        this.callback = callback;
+
+        this.easingType = easingType;
+        this.unit = unit;
+
+        this.cancelled = false;
+    }
+
+    cancel() {
+        this.cancelled = true;
+    }
+
+    update() {
+        throw new Error("Abstract Transformation can't be called!");
+    }
+
+    getCompletion() {
+        return Math.min(1, (window.performance.now() - this.startTime) / this.duration);
+    }
+
+    isComplete() {
+        return this.getCompletion() === 1 || this.cancelled;
+    }
+}
+
+export class TransformationElementStyle extends Transformation {
+    constructor(element, propertyName, start, destination, duration, callback = null, unit = "%", easingType = "linear") {
+        super(element, start, destination, duration, callback, unit, easingType);
+
+        this.start = start ? start : element.style[propertyName].substr(0, element.style[propertyName].length - unit.length);
+
+        this.propertyName = propertyName;
+    }
+
+    update() {
+        if(this.cancelled) return true;
+
+        let completion = this.getCompletion();
+
+        this.element.style[this.propertyName] = this.start + (this.destination - this.start) * MathUtil.ease(this.easingType, completion)+this.unit;
+
+        return completion === 1;
+    }
+}
+
+export class TransformationObjectField extends Transformation {
+    constructor(element, propertyName, start, destination, duration, callback = null, easingType = "linear") {
+        super(element, start, destination, duration, callback, "", easingType);
+
+        this.start = start ? start : element[propertyName];
+
+        this.propertyName = propertyName;
+    }
+
+    update() {
+        if(this.cancelled) return true;
+
+        let completion = this.getCompletion();
+
+        this.element[this.propertyName] = this.start + (this.destination - this.start) * MathUtil.ease(this.easingType, this.getCompletion())+this.unit;
+
+        return completion === 1;
+    }
+}
+
+export class TransformationObjectMethod extends Transformation {
+    constructor(element, propertyName, start, destination, duration, callback = null, easingType = "linear") {
+        super(element, start, destination, duration, callback, "", easingType);
+
+        this.propertyName = propertyName;
+    }
+
+    update() {
+        if(this.cancelled) return true;
+
+        let completion = this.getCompletion();
+
+        this.element[this.propertyName](this.start + (this.destination - this.start) * MathUtil.ease(this.easingType, this.getCompletion())+this.unit);
+
+        return completion === 1;
+    }
+}
